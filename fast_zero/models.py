57897459200dce60ba46ee3e8c5b25c78ec8a931
Carlos Petrikov <carlos.szarin@gmail.com>
2024-06-18 14:37:36 -0300

    initial commit

---
+++ fast_zero/models.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from sqlalchemy import func
+from sqlalchemy.orm import Mapped, mapped_column, registry
+
+table_registry = registry()
+
+
+@table_registry.mapped_as_dataclass  # converte a classe para dataclass
+class User:
+    __tablename__ = "users"
+
+    # Mapped: mapeia o atributo a uma coluna do DB
+    # mapped_column: aplica parametros nas colunas
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str]
+    email: Mapped[str] = mapped_column(unique=True)
+    created_at: Mapped[datetime] = mapped_column(
+        init=False, server_default=func.now()
+    )
