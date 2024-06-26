f41f958ded45d974898f970c6e1fad479eccb7be
Carlos Petrikov <carlos.szarin@gmail.com>
2024-06-26 11:19:28 -0300

    Implement refresh token and add relevant tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -91,9 +91,9 @@ def test_update_user(client, user, token):
     }
 
 
-def test_update_wrong_user_id(client, user, token):
+def test_update_user_with_wrong_user(client, other_user, token):
     response = client.put(
-        f"/users/{user.id + 1}",
+        f"/users/{other_user.id}",
         headers={"Authorization": f"Bearer {token}"},
         json={
             "username": "bob",
@@ -102,6 +102,7 @@ def test_update_wrong_user_id(client, user, token):
         },
     )
     assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {"detail": "Not enough permissions"}
 
 
 def test_delete_user(client, user, token):
@@ -113,6 +114,10 @@ def test_delete_user(client, user, token):
     assert response.json() == {"message": "User deleted"}
 
 
-def test_delete_wrong_user_id(client, user, token):
-    response = client.delete(f"/users/{user.id + 1}", headers={"Authorization": f"Bearer {token}"})
+def test_delete_user_wrong_user(client, other_user, token):
+    response = client.delete(
+        f"/users/{other_user.id}",
+        headers={"Authorization": f"Bearer {token}"},
+    )
     assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {"detail": "Not enough permissions"}
